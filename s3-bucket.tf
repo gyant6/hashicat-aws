9b428d66fbf80a81cce6aac0b5b199a1bbd4d353
Guangyan <guangyan_tang@hotmail.com>
2022-08-17 08:33:23 +0000

    FIXED

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-#   bucket_prefix = "gy"
+  bucket_prefix = "gy123"
   version = "2.8.0"
-  versioning = {
-    enabled = true
-  }
+
 
 }
